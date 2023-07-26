8603f5bdbc3d27dd9ff28f724ca3232fbb564c9f
SantinoViglino <SantinoViglino>
2023-07-26 19:50:13 -0300

    agregando start

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const PORT = process.env.PORT || 3000;
 const publicPath = path.resolve(__dirname, './public');
 
 app.use(express.static(publicPath));
-app.listen (3000, () => console.log ("servidor andando"));
+app.listen (PORT, () => console.log ("servidor andando en el puerto" + PORT));
 app.get ('/', function(req, res){
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
    });
