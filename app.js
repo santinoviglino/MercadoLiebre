5b13013ca780c13690e0ccdffbdc822cf3207843
SantinoViglino <SantinoViglino>
2023-07-27 01:04:06 -0300

    agregando el banner

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const publicPath = path.resolve(__dirname, './public');
 
 app.use(express.static(publicPath));
-app.listen (PORT, () => console.log ("servidor andando en el puerto" + PORT));
+app.listen (PORT, () => console.log ("servidor andando en el puerto: " + PORT));
 app.get ('/', function(req, res){
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
    });
